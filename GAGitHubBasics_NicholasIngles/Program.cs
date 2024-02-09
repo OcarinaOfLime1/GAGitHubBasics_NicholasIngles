5d2051fa5367a94707e385fe2c20b39c467a31ae
Nicholas Ingles <nringles@student.rtc.edu>
2024-02-08 22:31:52 -0800

    Updated Program File

--- GAGitHubBasics_NicholasIngles/Program.cs
+++ GAGitHubBasics_NicholasIngles/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Professor!");
+Console.WriteLine("Nicholas Ingles, I want to learn how to build games")
